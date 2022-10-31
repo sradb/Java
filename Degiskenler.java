6d9f18ffcc91aa2139dc2c764b7d08cc74ade467
sradb <siyaradibelli@gmail.com>
2022-10-31 19:27:09 +0200

    Create Degiskenler.java

---
+++ Degiskenler.java
@@ -0,0 +1,15 @@
+public class Degiskenler {
+    public static void main(String[] args) {
+        String yazi = "Yazı yazmak için kullanılır ";
+        int sayi = 15;
+        boolean a = true;
+        double ondalik = 3.15;
+        long tc = 22641123;
+        System.out.println(yazi);
+        System.out.println(sayi);
+        System.out.println(a);
+        System.out.println(ondalik);
+        System.out.println(tc);
+
+    }
+}
