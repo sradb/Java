12026e6610e8ad2773a5e20ef622aa445aab2e95
sradb <siyaradibelli@gmail.com>
2022-10-31 20:10:10 +0200

    Create doWhile.java

---
+++ doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int i =10;
+        do {
+            System.out.println(i);
+            System.out.println("Program 1 kere çalışır ve biter...");
+        }while(i>15);
+    }
+}
