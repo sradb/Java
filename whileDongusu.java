e4bc30bb01e3bec929b3896e3c1714658e3bf301
sradb <siyaradibelli@gmail.com>
2022-10-31 20:10:12 +0200

    Create whileDongusu.java

---
+++ whileDongusu.java
@@ -0,0 +1,11 @@
+public class whileDongusu {
+    public static void main(String[] args) {
+        //2'şer 2'şer artan sayılar...
+        int i =0;
+        while(i<=100) {
+            System.out.println(i);
+            i=i+2;
+
+        }
+    }
+}
