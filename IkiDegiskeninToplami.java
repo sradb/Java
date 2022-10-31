d749e17f1a88548a44b01e004739a66ffbb6a148
sradb <siyaradibelli@gmail.com>
2022-10-31 19:17:10 +0200

    Create IkiDegiskeninToplami.java

---
+++ IkiDegiskeninToplami.java
@@ -0,0 +1,12 @@
+public class IkiDegiskeninToplami {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 11;
+        System.out.println("A ve B toplamı : " + (a+b));
+
+        int c = 22;
+        int d = 33;
+        int toplam = c+d;
+        System.out.println("C ve D'nin toplamı : " + toplam);
+    }
+}
